4634bc48e1b8003ee30b96ffda6837c22474e480
EllaineD <danaiellaine@gmail.com>
2024-06-26 13:31:12 +0200

    UI touch-ups

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,7 +11,7 @@
     <link href="{{url('public/css/style.css') }}" rel="stylesheet" type="text/css">
 <link href="{{url('public/css/font-awesome.css') }}" rel="stylesheet" type="text/css">
 <link href="{{url('public/css/animate.css') }}" rel="stylesheet" type="text/css">
-<!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"> -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
    
 </head>
 <body>
@@ -20,7 +20,7 @@
 <div class="container">
         <nav class="navbar navbar-default" role="navigation">
                     <ul class="nav navbar-nav" id="mainNav">
-                    <li><div class="logo"><a href="#"><img src="{{ asset('images/favicon.png') }}" alt="logo"></a></div> </li>
+                    <li><div class="logo"><img src="{{ asset('images/favcon.png') }}" alt="logo"></div> </li>
                         <li><h1> <a href="#welcome" class="scroll-link">Business</h1></a></li>
                         
                         <li><a href="#story">Dashboard</a></li>
@@ -93,20 +93,21 @@
 
 
 <section id="services">
+<div class="section-title-container">
+    <h1 class="section-title">Services</h1>
+  </div>
   <div class="div_wrapper">
     <div class="container">
       <div class="row" id="title">
       <div class="col-12 text-center">
-  <div class="section-title-container">
-    <h1 class="section-title">Services</h1>
-  </div>
+  
 </div>
       </div>
       <div class="row">
         <div class="col-4">
           <div class="icon">
             <span class="icon-circle">
-              <img src="pill_icon.png" alt="Pill Icon">
+            <i class="bi bi-search" alt="Pill Icon"></i>
             </span>
             <h2 class="icon-title">Refreshing Design</h2>
             <p class="icon-paragraph">
@@ -117,7 +118,7 @@
         <div class="col-4">
           <div class="icon">
             <span class="icon-circle">
-              <img src="shield_icon.png" alt="Shield Icon">
+            <i class="bi bi-bootstrap-fill" alt="bootstrap"></i>
             </span>
             <h2 class="icon-title">Solid Bootstrap 5</h2>
             <p class="icon-paragraph">
@@ -128,7 +129,7 @@
         <div class="col-4">
           <div class="icon">
             <span class="icon-circle">
-              <img src="trumpet_icon.png" alt="Trumpet Icon">
+            <i class="bi bi-alarm" alt="bootstrap5"></i>
             </span>
             <h2 class="icon-title">100+ Components</h2>
             <p class="icon-paragraph">
@@ -141,7 +142,7 @@
         <div class="col-4">
           <div class="icon">
             <span class="icon-circle">
-              <img src="box_icon.png" alt="Box Icon">
+            <i class="bi bi-speedometer2" alt="speed"></i>
             </span>
             <h2 class="icon-title">Creative Solutions</h2>
             <p class="icon-paragraph">
@@ -152,7 +153,7 @@
         <div class="col-4">
           <div class="icon">
             <span class="icon-circle">
-              <img src="chart_icon.png" alt="Chart Icon">
+            <i class="bi bi-collection" alt="customise"></i>
             </span>
             <h2 class="icon-title">Data Analysis</h2>
             <p class="icon-paragraph">
@@ -163,7 +164,7 @@
         <div class="col-4">
           <div class="icon">
             <span class="icon-circle">
-              <img src="tools_icon.png" alt="Tools Icon">
+               <i class="bi bi-arrow-clockwise" alt="upgrade"></i>
             </span>
             <h2 class="icon-title">Effective Tools</h2>
             <p class="icon-paragraph">
@@ -177,12 +178,13 @@
 </section>
 
 <section id="prices">
+<div class="section-title-container">
+    <h1 class="section-title">Prices</h1>
+  </div>
   <div class="container">
   <div class="row" id="title">
   <div class="col-12 text-center">
-  <div class="section-title-container">
-    <h1 class="section-title">Prices</h1>
-  </div>
+  
       </div>
     <div class="row">
       <div class="col">
